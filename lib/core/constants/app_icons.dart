dd62a4b6fbf5b10abc0d1b8febcb27f787f9e61c
SubeoHocJava <165917719+SubeoHocJava@users.noreply.github.com>
2025-11-01 23:19:27 +0700

    Refactor: Clean up unused icon assets and update theme colors

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,31 +1,4 @@
 class AppAssets {
   static const _base = 'assets/icons';
   static const logo       = '$_base/logo.png';
-  static const user       = '$_base/user.png';
-  static const sun        = '$_base/sun.png';
-  static const upload     = '$_base/upload.png';
-  static const avt        = '$_base/avt.png';
-
-  // Bottom bar
-  static const home       = '$_base/home.png';
-  static const lib        = '$_base/lib.png';
-  static const explore    = '$_base/explore.png';
-  static const bell       = '$_base/bell.png';
-  static const homePick   = '$_base/home_pick.png';
-  static const libPick    = '$_base/lib_pick.png';
-  static const explorePick= '$_base/explore_pick.png';
-  static const bellPick   = '$_base/bell_pick.png';
-
-  // Actions
-  static const download   = '$_base/download.png';
-  static const saved      = '$_base/saved.png';
-  static const cmt        = '$_base/cmt.png';
-  static const like       = '$_base/like.png';
-
-  // Khác
-  static const folder     = '$_base/folder.png';
-  static const school     = '$_base/school.png';
-  static const numPage    = '$_base/num_page.png';
-  static const time       = '$_base/time.png';
-  static const nav        = '$_base/nav.png';
 }
