e184172ad122fdd84e0f444c375705606ca3278a
Your Name <203855345+minhhien-e@users.noreply.github.com>
2025-11-15 23:08:36 +0700

    feat(dto): thêm api response chuẩn hóa response từ be

---
+++ lib/data/model/api_response.dart
@@ -0,0 +1,17 @@
+class ApiResponse<T> {
+  final int statusCode;
+  final int errorCode;
+  final String traceId;
+  final T data;
+
+  ApiResponse(this.statusCode, this.errorCode, this.traceId, this.data);
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json) {
+    return ApiResponse(
+      json['statusCode'] as int,
+      json['errorCode'] as int,
+      json['traceId'] as String,
+      json['data'] as T,
+    );
+  }
+}
