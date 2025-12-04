1a2440801218056231b517154862d8c790f5b670
lambaoduy <22130057@st.hcmuad.edu.vn>
2025-12-04 14:41:23 +0700

    feature: xử lý logic cho trang upload file

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:studydocs/features/library/presentation/screen/library_screen.dart';
-import 'package:studydocs/features/profile/presentation/screen/profile_screen.dart';
-import 'package:studydocs/features/upload_file/presentation/screen/upload_file_screen.dart';
-
-
-
 import 'core/theme/app_theme.dart';
 import 'features/subject_library/presentation/screen/subject_library_screen.dart';
+import 'features/upload_file/presentation/screen/upload_file_screen.dart';
+
 
 
 class MyApp extends StatelessWidget {
