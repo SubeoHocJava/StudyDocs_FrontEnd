d2a56482e1e862c670d8d926920ec365b103a39a
SubeoHocJava <165917719+SubeoHocJava@users.noreply.github.com>
2026-01-03 19:44:28 +0700

    Refactor MainScreen to use StatelessWidget, enhance navigation logic, and integrate Header component

--- lib/features/main/main_screen.dart
+++ lib/features/main/main_screen.dart
@@ -1,70 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:studydocs/core/widgets/bottom_nav.dart';
+import 'package:studydocs/core/widgets/header.dart';
 import 'package:studydocs/features/home/presentation/home_screen.dart';
 import 'package:studydocs/features/library/presentation/screen/library_screen.dart';
 import 'package:studydocs/features/subject_library/presentation/screen/subject_library_screen.dart';
 import 'package:studydocs/features/notification/presentation/screen/notification_screen.dart';
 
-class MainScreen extends StatefulWidget {
-  final int initialIndex;
-  const MainScreen({super.key, this.initialIndex = 0});
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key, required this.navigationShell});
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
+  final StatefulNavigationShell navigationShell;
 
-class _MainScreenState extends State<MainScreen> {
-  late int _currentIndex;
-  late PageController _pageController;
-
-  final List<Widget> _screens = [
-    HomePage(),
-    const LibraryScreen(),
-    SubjectLibraryScreen(),
-    const NotificationScreen(userId: ''),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _currentIndex = widget.initialIndex;
-    _pageController = PageController(initialPage: _currentIndex);
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-    _pageController.animateToPage(
+  void _goBranch(BuildContext context, int index) {
+    navigationShell.goBranch(
       index,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
+      initialLocation: index == navigationShell.currentIndex,
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final currentIndex = navigationShell.currentIndex;
+
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: _screens,
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        physics: const NeverScrollableScrollPhysics(), // Disable swipe to change pages
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Header(
+              isDefault: true,
+              selectedIndex: currentIndex,
+              onLogoTap: () {
+                navigationShell.goBranch(0, initialLocation: true);
+              },
+            ),
+            Expanded(child: navigationShell),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNav(
-        currentIndex: _currentIndex,
-        onTap: _onTap,
+        currentIndex: currentIndex,
+        onTap: (i) => _goBranch(context, i),
       ),
     );
+  }
+}
+
+class MainTabHomePage extends StatelessWidget {
+  const MainTabHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
+
+class MainTabLibraryPage extends StatelessWidget {
+  const MainTabLibraryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LibraryScreen();
+  }
+}
+
+class MainTabExplorePage extends StatelessWidget {
+  const MainTabExplorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SubjectLibraryScreen();
+  }
+}
+
+class MainTabNotificationsPage extends StatelessWidget {
+  const MainTabNotificationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const NotificationScreen(userId: '');
   }
 }
