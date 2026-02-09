07e8e55edf4b8e828cd42f3ba7c646b5bd274153
lambaoduy <22130057@st.hcmuaf.edu.vn>
2026-02-09 15:00:07 +0700

    dev: initial ver2 project

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,22 @@
+class AppAssets {
+  static const _base = 'assets/icons';
+  static const logo       = '$_base/logo.png';
+  static const logoDark   = '$_base/logo_dark.png';
+  // Notification
+  static const markAsRead = '$_base/mark_as_read.png';
+  static const moreHoriz = '$_base/more_horiz.png';
+  static const like = '$_base/like.png';
+  static const download = '$_base/download.png';
+  static const bin = '$_base/bin.png';
+
+  static const folder   = '$_base/folder.png';
+  static const school   = '$_base/school.png';
+
+  static const saved     = '$_base/saved.png';
+  static const unsaved   = '$_base/unsaved.png';
+
+  static const avt       = '$_base/avatar.png';
+
+  static const cmt = '$_base/cmt.png';
+  static const upload = '$_base/upload.png';
+}
