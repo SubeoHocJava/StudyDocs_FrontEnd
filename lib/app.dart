8b359ae05ccafb586215c34a512164fb8c05a1ce
lambaoduy <22130057@st.hcmuaf.edu.vn>
2026-02-18 16:24:10 +0700

    dev: initial ver2 update infor form dialog

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'core/widgets/feat/update_infor_form/logic/update_infor_bloc.dart';
+import 'core/widgets/feat/update_infor_form/presentation/UpdateInforDialog.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -10,10 +12,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => UpdateInforBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const TestScreen(),
+      ),
+    );
+  }
+}
 
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Test giao diện")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              barrierDismissible: true,
+              builder: (_) {
+                return BlocProvider.value(
+                  value: context.read<UpdateInforBloc>(),
+                  child: const Dialog(
+                    backgroundColor: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: UpdateInforDialog(),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          child: const Text("Mở form cập nhật"),
+        ),
+      ),
+    );
   }
 }
