ad26bc07b6118f6d04f7ad8c316767ccfb823904
LYTDUNG <22130054@st.hcmuaf.edu.vn>
2026-03-24 20:06:14 +0700

    Standardize AppAssets

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,30 +1,46 @@
 class AppAssets {
   static const _base = 'assets/icons';
+
+  // Logo
   static const logo       = '$_base/logo.png';
   static const logoDark   = '$_base/logo_dark.png';
-  // Notification
+
+  // Common Actions
   static const markAsRead = '$_base/mark_as_read.png';
-  static const moreHoriz = '$_base/more_horiz.png';
-  static const like = '$_base/like.png';
-  static const download = '$_base/download.png';
-  static const bin = '$_base/bin.png';
+  static const moreHoriz  = '$_base/more_horiz.png';
+  static const download   = '$_base/download.png';
+  static const bin        = '$_base/bin.png';
+  static const upload     = '$_base/upload.png';
 
-  static const notiLike = '$_base/noti_like.png';
-  static const notiComment = '$_base/noti_comment.png';
-  static const notiDownload = '$_base/noti_download.png';
-  static const notiSaved = '$_base/noti_saved.png';
-  static const notiMarkAsRead = '$_base/noti_markasread.png';
-  static const notiTrash = '$_base/noti_trash.png';
-  static const notiRestore = '$_base/noti_restore.png';
+  // Like
+  static const like        = '$_base/like.png';
+  static const outlineLike = '$_base/like.png';      // giữ để backward compatibility
+  static const fullLike    = '$_base/full_like.png';
 
+  // Navigation / UI
+  static const chevronDown = '$_base/chevron_down.png';
+
+  // Domain Icons
   static const folder   = '$_base/folder.png';
   static const school   = '$_base/school.png';
+  static const numPages = '$_base/num_page.png';
 
-  static const saved     = '$_base/saved.png';
-  static const unsaved   = '$_base/unsaved.png';
+  // Save
+  static const saved   = '$_base/saved.png';
+  static const unsaved = '$_base/unsaved.png';
 
-  static const avt       = '$_base/avatar.png';
+  // User
+  static const avt = '$_base/avatar.png';
 
+  // Comment
   static const cmt = '$_base/cmt.png';
-  static const upload = '$_base/upload.png';
+
+  // Notification Icons
+  static const notiLike        = '$_base/noti_like.png';
+  static const notiComment     = '$_base/noti_comment.png';
+  static const notiDownload    = '$_base/noti_download.png';
+  static const notiSaved       = '$_base/noti_saved.png';
+  static const notiRestore     = '$_base/noti_restore.png';
+  static const notiTrash       = '$_base/noti_trash.png';
+  static const notiMarkAsRead  = '$_base/noti_markasread.png';
 }
